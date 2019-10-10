a0e1fd05880ef0f8cd29f448c195c21a6bb4ee1f
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-10 14:38:58 -0600

    added exits
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -27,18 +27,31 @@ namespace ConsoleAdventure.Project.Models
       Room vw = new Room("vw", "Room with a view");
       Room str = new Room("str", "Stair way");
       Room wr = new Room("wr", "Weapons Room");
+      Room br = new Room("br", "Bell Room"); 
 
       Dictionary<string, string> Exits = new Dictionary<string, string>();
-      Exits.Add("north", "");
+      ew.Exits.Add("north", mh);
+      mh.Exits.Add("east", vw);
+      mh.Exits.Add("west", wr);
+      mh.Exits.Add("north", str);
+      mh.Exits.Add("south", ew);
+      vw.Exits.Add("west", mh); 
+      wr.Exits.Add("east", mh);
+      wr.Exits.Add("east", mh);
+      str.Exits.Add("north", br);
+      str.Exits.Add("south", mh);
+      br.Exits.Add("north", twr);
+      twr.Exits.Add("south", br); 
 
 
-      //room connections 
-      ew.AddConnection(mh);
-      mh.AddConnection(str);
-      str.AddConnection(twr);
-      twr.AddConnection(vw);
-      mh.AddConnection(wr);
-      wr.AddConnection(str);
+
+    //   //room connections 
+    //   ew.AddConnection(mh);
+    //   mh.AddConnection(str);
+    //   str.AddConnection(twr);
+    //   twr.AddConnection(vw);
+    //   mh.AddConnection(wr);
+    //   wr.AddConnection(str);
 
       //creating items
       Item torch = new Item("lights the room");
