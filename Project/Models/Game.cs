7deceb0a24a7c51d644dda4372107a61bed74ba3
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-10 14:08:33 -0600

    exit dictionary
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -1,33 +1,38 @@
+using System.Collections.Generic;
 using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Game : IGame
-    {
+  public class Game : IGame
+  {
     public Game()
     {
     }
 
-    public Game(IRoom currentRoom, IPlayer currentPlayer) 
-        {
-          this.CurrentRoom = currentRoom;
-              this.CurrentPlayer = currentPlayer;
-               
-        }
-                public IRoom CurrentRoom { get; set; }
-        public IPlayer CurrentPlayer { get; set; }
+    public Game(IRoom currentRoom, IPlayer currentPlayer)
+    {
+      this.CurrentRoom = currentRoom;
+      this.CurrentPlayer = currentPlayer;
 
-        //NOTE Make yo rooms here...
-        public void Setup()
-        { //rooms 
-            Room ew = new Room("ew", "entry way");
-            Room mh = new Room("mh", "main halll");
-            Room twr = new Room("twr", "tower");
+    }
+    public IRoom CurrentRoom { get; set; }
+    public IPlayer CurrentPlayer { get; set; }
+
+    //NOTE Make yo rooms here...
+    public void Setup()
+    { //rooms 
+      Room ew = new Room("ew", "entry way");
+      Room mh = new Room("mh", "main halll");
+      Room twr = new Room("twr", "tower");
       Room vw = new Room("vw", "Room with a view");
       Room str = new Room("str", "Stair way");
-      Room wr = new Room("wr", "Weapons Room"); 
+      Room wr = new Room("wr", "Weapons Room");
 
-//room connections 
+      Dictionary<string, string> Exits = new Dictionary<string, string>();
+      Exits.Add("north", "");
+
+
+      //room connections 
       ew.AddConnection(mh);
       mh.AddConnection(str);
       str.AddConnection(twr);
@@ -36,23 +41,23 @@ namespace ConsoleAdventure.Project.Models
       wr.AddConnection(str);
 
       //creating items
-      Item torch = new Item("lights the room"); 
+      Item torch = new Item("lights the room");
 
       // Items in rooms
-      ew.Items.Add(torch); 
+      ew.Items.Add(torch);
 
       //starting room 
-      CurrentRoom = ew; 
+      CurrentRoom = ew;
 
 
 
 
       throw new System.NotImplementedException();
-        }
+    }
     //     public Game()
     //     {
     //   Room = new Room();
     //   Setup(); 
-    }
-    }
-    
+  }
+}
+
